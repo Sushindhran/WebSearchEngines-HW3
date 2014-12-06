4c8e0eec110ccca14932ecdd55ed6afc87a0a9b4
Harikrishnan <sushindhran@gmail.com>
2014-12-06 13:15:44 -0500

    Client almost done

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = function gruntFile(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        shell: {
+            //application scripts
+            serve: {
+                command: ' java -cp lib/jsoup-1.8.1.jar:src: -Xmx2048m edu.nyu.cs.cs2580.SearchEngine \ --mode=serve --port=25810 --options=conf/engine.conf',
+                options: {
+                    async: false
+                }
+            },
+            mine: {
+                command: 'java lib/jsoup-1.8.1.jar:src: -Xmx2048m edu.nyu.cs.cs2580.SearchServer \ --mode=mining --options=conf/engine.conf',
+                options: {
+                    async: false
+                }
+            },
+            index: {
+                command: 'java -cp lib/jsoup-1.8.1.jar:src: -Xmx2048m edu.nyu.cs.cs2580.SearchEngine \ --mode=index --options=conf/engine.conf',
+                options: {
+                    async: false
+                }
+            },
+            compile: {
+                command: 'javac -cp lib/Jsoup-1.8.1.jar src/edu/nyu/cs/cs2580/*.java',
+                options: {
+                    async: false
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('compile', [
+        'shell:compile'
+    ]);
+
+    grunt.registerTask('serve', [
+       'shell:serve'
+    ]);
+
+    grunt.registerTask('serve', [
+        'shell:mine'
+    ]);
+
+    grunt.registerTask('serve', [
+        'shell:index'
+    ]);
+};
---
+++ client/src/main/application/application.js
@@ -0,0 +1,40 @@
+/** This file defines all the dependencies that the AngularJS application will use
+ *  like filters, services, directives and controllers.
+ */
+
+var WebSearchEngines = WebSearchEngines || {};
+
+WebSearchEngines.Constants = angular.module('websearchengines.constants', []);
+WebSearchEngines.Services = angular.module('websearchengines.services', []);
+WebSearchEngines.Controllers = angular.module('websearchengines.controllers', []);
+WebSearchEngines.Filters = angular.module('websearchengines.filters', []);
+WebSearchEngines.Directives = angular.module('websearchengines.directives', []);
+WebSearchEngines.Dependencies = [
+    'websearchengines.filters',
+    'websearchengines.services',
+    'websearchengines.directives',
+    'websearchengines.constants',
+    'websearchengines.controllers',
+    'ngRoute'
+];
+
+//Define the routes for the application using $routeProvider
+angular.module(
+    'websearchengines',
+    WebSearchEngines.Dependencies
+).
+    config(
+    [
+        '$routeProvider',
+        function($routeProvider) {
+
+            //Routes to game.html for /play
+            $routeProvider.when('/search', {
+                templateUrl: '/WebSearchEngines/src/main/application/view/search.html'
+            });
+
+            //Defaults to game.html if it doesn't match any route
+            $routeProvider.otherwise({ templateUrl: '/WebSearchEngines/src/main/application/view/search.html' });
+        }
+    ]
+);
